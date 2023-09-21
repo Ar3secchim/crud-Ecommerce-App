7f95f39085ac4ffc1be819f231c080d41bf4d3e4
Renara Sechim <renarasecchim@gmail.com>
2023-09-21 20:40:54 -0300

    feat: update

--- application/src/main/java/com/crud/ecommerce/app/application/repository/RepositoryException.java
+++
@@ -1,9 +0,0 @@
-package com.crud.ecommerce.app.application.repository;
-
-public class RepositoryException extends RuntimeException {
-
-  public RepositoryException(String message, Exception cause) {
-    super(message, cause);
-  }
-
-}
--- domain/src/main/java/com/crud/ecommerce/app/domain/OrderStatus.java
+++
@@ -1,7 +0,0 @@
-package com.crud.ecommerce.app.domain;
-
-public enum OrderStatus {
-
-  OPEN, PENDING_PAYMENT, PAID, SHIPPING, FINISH;
-
-}
