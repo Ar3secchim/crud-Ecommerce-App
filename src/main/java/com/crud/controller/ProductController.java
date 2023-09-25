8d56e54757c994aa0bb57cf3ab2a81244294059c
Renara Sechim <renarasecchim@gmail.com>
2023-09-25 15:13:40 -0300

    feat: add ProductController get

--- src/main/java/com/crud/controller/ProductController.java
+++ src/main/java/com/crud/controller/ProductController.java
@@ -6,18 +6,28 @@ import com.crud.model.Product;
 import com.crud.usecases.impl.ProductUseCaseImpl;
 import com.crud.utils.ProductConvert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
   @Autowired
   ProductUseCaseImpl productService;
 
-  @PostMapping ("product")
+  @PostMapping ("/product")
   public ProductResponse createCustomer(@RequestBody ProductRequest productRequest){
     Product product =  productService.create(ProductConvert.toEntity(productRequest));
     return ProductConvert.toResponse(product);
   }
+
+  @GetMapping("/product")
+  public List<ProductResponse> getProduct(){
+    List<Product> listProduct = productService.listAll();
+    return ProductConvert.toListResponse(listProduct);
+  }
+
 }
