f0cef57b9e0996bbae0c1b811f7768994a3bab6b
Renara Sechim <renarasecchim@gmail.com>
2023-09-29 19:26:21 -0300

    feat: update valitadion request

--- src/main/java/com/crud/controller/dto/OrderItem/OrderItemRequest.java
+++ src/main/java/com/crud/controller/dto/OrderItem/OrderItemRequest.java
@@ -1,10 +1,12 @@
 package com.crud.controller.dto.OrderItem;
 
-import com.crud.model.Product;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class OrderItemRequest {
+  @NotBlank()
   private Integer product;
+  @NotBlank()
   private Integer amount;
 }
--- src/main/java/com/crud/controller/dto/product/ProductRequest.java
+++ src/main/java/com/crud/controller/dto/product/ProductRequest.java
@@ -1,5 +1,6 @@
 package com.crud.controller.dto.product;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +9,9 @@ import java.math.BigDecimal;
 @Setter @Getter
 public class ProductRequest {
   private Integer id;
+  @NotBlank()
   private String name;
   private String description;
+  @NotBlank()
   private Double price;
 }
