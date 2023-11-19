2173694031602c5fbaae871f2889df0a4591b012
Renara Secchim <renarasecchim@gmail.com>
2023-11-19 11:49:56 -0300

    chore: config db test h2

--- src/test/java/com/crud/CrudApplicationTests.java
+++ src/test/java/com/crud/CrudApplicationTests.java
@@ -2,12 +2,13 @@ package com.crud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CrudApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
