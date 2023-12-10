40006f1d87e72bfb3b2d87862c0e117ad2ea2cf9
Ar3secchim <renarasecchim@gmail.com>
2023-12-10 05:48:55 -0300

    feat: create test for FindController and GetAllController

---
+++ src/test/java/com/crud/modules/integration/customers/controller/CustomerGetAllControllerIntegrationTest.java
@@ -0,0 +1,60 @@
+package com.crud.modules.integration.customers.controller;
+
+import com.crud.modules.customers.DTO.CustomerResponse;
+import com.crud.modules.customers.entity.Customer;
+import com.crud.modules.customers.usecase.ListCustomer;
+import com.crud.modules.customers.usecase.RegisterCustomer;
+import com.crud.utils.CustomerConvert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomerGetAllControllerIntegrationTest {
+  @MockBean
+  private ListCustomer listCustomer;;
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @BeforeEach
+  void setup() throws Exception {
+    Customer customer = new Customer();
+    customer.setIdTransaction(UUID.randomUUID().toString());
+    customer.setName("int-test");
+    customer.setEmail("int-test@gmail.com");
+    List<CustomerResponse> customerResponse = CustomerConvert.toListResponse(List.of(customer));
+    Mockito.when(listCustomer.execute()).thenReturn(customerResponse);
+  }
+
+
+  @Test
+  void customerGetAllWithSuccess() throws Exception {
+    mockMvc.perform(
+            MockMvcRequestBuilders.get("/customer")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+    ).andDo(
+            MockMvcResultHandlers.print()
+    ).andExpect(
+            MockMvcResultMatchers.status().is2xxSuccessful()
+    ).andExpect(
+            MockMvcResultMatchers.jsonPath("$[0].name")
+                    .value("int-test")
+    );
+  }
+}
